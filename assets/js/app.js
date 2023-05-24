a20ed223e8bd9704a086934e478a1c2bb4799764
Youssef Ghareb <youssef.ghareb99@gmail.com>
2023-05-24 16:53:33 +0300

    code

---
+++ assets/js/app.js
@@ -0,0 +1,138 @@
+const data = [
+    {
+        id: 1,
+        title: "egypt tangbia",
+        description: "card 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card1.jpg"
+    },
+    {
+        id: 2,
+        title: "ollin phisotip",
+        description: "card 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card2.jpg"
+    },
+    {
+        id: 3,
+        title: "coria dyna",
+        description: "card 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card3.jpg"
+    },
+    {
+        id: 4,
+        title: "synse of dubia",
+        description: "card 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card4.jpg"
+    },
+    {
+        id: 5,
+        title: "egypt tangbia",
+        description: "card 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card5.jpg"
+    },
+    {
+        id: 6,
+        title: "ollin phisotip",
+        description: "card 6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card6.jpg"
+    },
+    {
+        id: 7,
+        title: "coria dyna",
+        description: "card 7 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card7.jpg"
+    },
+    {
+        id: 8,
+        title: "synse of dubia",
+        description: "card 8 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card8.jpg"
+    },
+    {
+        id: 9,
+        title: "egypt tangbia",
+        description: "card 9 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card9.jpg"
+    },
+    {
+        id: 10,
+        title: "ollin phisotip",
+        description: "card 10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card10.jpg"
+    },
+    {
+        id: 11,
+        title: "coria dyna",
+        description: "card 11 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card11.jpg"
+    },
+    {
+        id: 12,
+        title: "synse of dubia",
+        description: "card 12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda explicabo velit, unde maiores mollitia quas. Sit quod necessitatibus perferendis asperiores laboriosam officiis nobis quos, doloremque labore omnis ut accusantium, nemo autem deleniti. Atque accusantium autem animi sint reiciendis, voluptatum ipsum harum totam velit. Magni mollitia quisquam optio, repellendus numquam iste veniam illum exercitationem molestias, nulla reiciendis. Amet sequi debitis quaerat minima veritatis expedita, nostrum repellat dolor ipsam, ut culpa id adipisci vitae non excepturi modi autem architecto totam, ea iste?",
+        imgSrc: "card12.jpg"
+    },
+];
+
+function cardClick(btn) {
+    let card = data.find(x => x.id == btn.dataset.id);
+    $(".modal-body").html(`
+        <div class="row align-items-center">
+            <div class="col-md-5">
+                <img src="assets/img/${card.imgSrc}" class="img-fluid">
+            </div>
+        <div class="col-md-7">
+                <div class="modal-title display-5">${card.title}</div>
+                <div class="modal-description">${card.description}</div>
+            </div>
+        </div>
+    `);
+    $("#cardModal").show();
+}
+
+function truncateText(str, charLimit) {
+    var truncatedText;
+    if (str.length >= charLimit) {
+        truncatedText = str.substring(0, charLimit - 1);
+        truncatedText += " ...";
+        return truncatedText;
+    } else {
+        return str;
+    }
+}
+
+$(document).ready(function () {
+    for (let i = 0; i < data.length; i++) {
+        $("#data-row").append(`
+            <div class="col-md-3">
+                <div class="site-card" onclick="cardClick(this)" data-id=${data[i].id}>
+                    <div class="site-card-imgCont m-0 p-0">
+                        <img src="assets/img/${data[i].imgSrc}" class="img-fluid">
+                    </div>
+                    <div class="site-card-body text-center px-4 py-3">
+                        <div class="site-card-body-heading">${data[i].title}</div>
+                        <div class="site-card-body-description">${truncateText(data[i].description, 30)}</div>
+                        <div class="site-card-body-info d-flex justify-content-between">
+                            <div class="d-flex gap-1 align-items-center">
+                                <i class="fa-regular fa-calendar"></i>
+                                <div class="site-card-body-info-label">
+                                    Aug 5, 2013
+                                </div>
+                            </div>
+                            <div class="d-flex gap-1 align-items-center">
+                                <i class="fa-regular fa-comments"></i>
+                                <div class="site-card-body-info-label">
+                                    22 comments
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
+    }
+
+    $("#modalClose").click(function(){
+        $("#cardModal").hide();
+    })
+
+})
